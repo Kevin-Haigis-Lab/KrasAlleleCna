df33b2ec3518a5bb8a095b50923ae1d25319c4a6
jhrcook <joshuacook0023@gmail.com>
2019-04-07 19:32:25 -0400

    data: parse VCF data and stored for use

---
+++ R/parse_vcf.R
@@ -0,0 +1,39 @@
+#' Parse a VCF file
+#'
+#' @description Parse the read-depth information of a VCF file.
+#'
+#' @param fname path to the file
+#'
+#' @return a tibble
+#'
+#' @examples
+#' # TODO
+#'
+#' @importFrom stringr str_split_fixed str_remove_all
+#' @export parse_vcf
+parse_vcf <- function(fname) {
+    # find the start of the data in the VCF file and read
+    header_line <- grep("#CHROM", readLines(fname)) - 1
+    v <- readr::read_tsv(fname,
+                         col_type = "cdcccdcccc",
+                         skip = header_line)
+    colnames(v) <- c("chr", "start", "id", "ref", "alt", "qual", "filter",
+                     "info", "format", "read_info")
+    # breakout the information held in FORMAT
+    # FORMAT = PL:DP:AD
+        #  x PL = "List of Phred-scaled genotype likelihoods"
+        #  * DP = "Number of high-quality bases"
+        #  * AD = "Allelic depths"
+    v %<>%
+        dplyr::select(start, ref, alt, qual, read_info) %>%
+        dplyr::mutate(num_high_qual_bases = str_split_fixed(read_info, ":", 3)[, 2],
+                      alleleic_depths = str_split_fixed(read_info, ":", 3)[, 3],
+                      alleleic_depths = str_remove_all(alleleic_depths, ",0$"))
+    return(v)
+}
+
+
+
+utils::globalVariables(c("start", "ref", "alt", "qual", "read_info",
+                         "alleleic_depths"),
+                       add = TRUE)

1f412e5eda0fcd2a901b016ed125647c251147b5
jhrcook <joshuacook0023@gmail.com>
2019-04-14 12:14:05 -0400

    style: removed some empty space

--- R/utilities.R
+++ R/utilities.R
@@ -1,14 +1,14 @@
 #' Number of unique values
-#' 
+#'
 #' Returns the number of unique values in a vector
-#' 
+#'
 #' @param x vector
-#' 
+#'
 #' @return integer
-#' 
+#'
 #' @examples
 #' a <- c(1,1,2,3,4)
 #' n_unique(a)
-#' 
+#'
 #' @export n_unique
 n_unique <- function(x) length(unique(x))
